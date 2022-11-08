9acf589d879e0b4f7e4b0bc729543a8d912497b5
Jose Alejandro Montoya Gonzalez <jmontoyag@premexm.loc>
2022-11-08 16:58:27 -0500

    new data

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
 #Python
 from typing import Optional
-
+from enum import Enum #Para poder definir la validaciones
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 #FastAPI
-from fastapi import FastAPI ,Body, Query
+from fastapi import FastAPI ,Body, Query, Path
 
 app = FastAPI() #Instancia de FastAPI 
 
 #Models
-class Person(BaseModel):
-    first_name:str
-    last_name:str
-    age:int
-    hair_color:Optional[str] = None
-    is_married:Optional[bool] = None
 
+
+class HairColor(str,Enum): #Hereda de enum, el str permite que se visualice mejor en swagger
+    white = "white"
+    brown = "brown"
+    black = "black"
+
+class Person(BaseModel):
+    first_name:str = Field(
+        ...,
+        min_length=1,
+        max_length=100,
+        example="Alejo"
+        ) #Para validar el parametro del modelo
+    last_name:str = Field(
+        ...,
+        min_length=1,
+        max_length=100,
+        example="Montoya"
+        ) 
+    age:int = Field(
+        ...,
+        gt=0,
+        le=80,
+        example=23
+        ) 
+    hair_color:Optional[HairColor] = Field(default=None)
+    is_married:Optional[bool] = Field(default=None)
+    
+
+    # class Config:
+    #     schema_extra={ #Para enviar una repsuesta por defecto
+    #         "example": {
+    #             "first_name": "Rodrigo",
+    #             "last_name": "Lopez",
+    #             "age": 30,
+    #             "hair_color": "black",
+    #             "is_married": False
+    #         }
+    #     }
+
+class Location(BaseModel):
+    city:str
+    state:str
+    country:str
+
+#Methods
 @app.get("/")
 def get():
     return {
@@ -32,7 +72,48 @@ def create_person(person: Person = Body(...)): ## los ... ellipsis indican que e
 
 @app.get("/person/detail")
 def show_person(
-    name:Optional[str] = Query(None, min_length=1, max_length=50),
-    age: int = Query(...) #Lo hacemos obligatorio, sin embargo con query no es lo ideal manejarlo así
+    name:Optional[str] = Query(
+        None, 
+        min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: int = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's required"
+        ) #Lo hacemos obligatorio, sin embargo con query no es lo ideal manejarlo así
 ):
     return {name:age}
+
+#Validaciones path parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id:int = Path(
+        ..., 
+        gt=0,
+        title="Person ID",
+        description="This is the person id. It's greater than 0"
+        ),
+):
+    return {person_id:"Existe"}
+
+#Validaciones path parameters
+@app.put("/person/{person_id}")
+def update_person(
+    person_id:int = Path(
+        ..., 
+        gt=0,
+        title="Person ID",
+        description="This is the person id. It's greater than 0"
+    ),
+    person:Person = Body(...),
+    location:Location = Body(...)
+):
+    return {
+        "person":person.dict(),
+        "location":location.dict()
+    }
+
+
